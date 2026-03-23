2e7497ba90b762dfd0b5d45c3db70c2b0d5e956b
Kendall Smith <kendall.b.smith@gmail.com>
2026-03-23 08:46:11 -0500

    Initial Commit

---
+++ CursorLock/CursorLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursorLockApp.swift
+//  CursorLock
+//
+//  Created by Kendall Smith on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct CursorLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
